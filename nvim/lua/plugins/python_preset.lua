7de85cce6b6dcec20227fff507c56dc23c0afe37
HizKz <hizume0308@gmail.com>
2026-06-26 14:11:46 +0900

    Add Python development preset

---
+++ nvim/lua/plugins/python_preset.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                extraPaths = { "./" },
+                typeCheckingMode = "basic",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "pyright",
+        "ruff",
+      })
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.python = { "ruff_organize_imports", "ruff_format" }
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.python = { "ruff" }
+    end,
+  },
+}
